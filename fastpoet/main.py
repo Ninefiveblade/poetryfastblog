ff413f0a95b15ad5088992e2f7c87bc2ca1b7da9
Ninefiveblade <cherep1.92@yandex.ru>
2022-04-29 12:17:18 +0300

    Подтверждение изменений

--- fastpoet/main.py
+++ fastpoet/main.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import Session
 
 from fastpoet import crud, models, schemas
 
-from database import SessionLocal, engine
+from .database import SessionLocal, engine
 
 models.Base.metadata.create_all(bind=engine)
 
