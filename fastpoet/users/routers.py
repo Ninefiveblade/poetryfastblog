e80c624d6f543c8373809287783e52d0faf53291
LeAries <learies@ya.ru>
2022-05-09 06:51:32 +0300

    Add get user by username and docstring

--- fastpoet/users/routers.py
+++ fastpoet/users/routers.py
@@ -6,7 +6,7 @@ from fastpoet.settings import models
 from fastpoet.settings.database import SessionLocal, engine
 
 from .schemas import User, UserCreate
-from .service import add_user, get_user, get_users
+from .service import add_user, get_user, get_user_by_username, get_users
 
 router = APIRouter()
 
@@ -23,14 +23,23 @@ def get_db():
 
 @router.get("/users/", response_model=list[User])
 def users_get(db: Session = Depends(get_db)):
+    """Get users"""
     users = get_users(db)
     return users
 
 
 @router.get("/users/{user_id}", response_model=User)
 def user_get(user_id: int, db: Session = Depends(get_db)):
-    users = get_user(db, user_id)
-    return users
+    """Get user by id"""
+    user = get_user(db, user_id)
+    return user
+
+
+@router.get("/user/{username}", response_model=User)
+def user_get(username: str, db: Session = Depends(get_db)):
+    """Get user by username"""
+    user = get_user_by_username(db, username)
+    return user
 
 
 @router.post("/users/", response_model=User)
