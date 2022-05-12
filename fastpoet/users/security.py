6e6415daa377295ca8baf3303a4938f9958511fd
Ninefiveblade <cherep1.92@yandex.ru>
2022-05-12 09:12:02 +0300

    test

--- fastpoet/users/security.py
+++ fastpoet/users/security.py
@@ -13,26 +13,26 @@ oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 User = models.User
 
 
-def get_password_hash(password: str):
+def get_password_hash(password: str):  # хешируем пароль
     return pwd_context.hash(password)
-
-
-def get_user(username: str, db: Session = Depends(get_db)):
-    if username in db:
-        user_dict = get_user_by_username(username)
-        return UserInDB(**user_dict)
 
 
 class UserInDB(User):
     hashed_password: str
 
 
-def fake_decode_token(token):
+def get_user(username: str, db: Session = Depends(get_db)):  # полученем юзера по нику
+    if username in db:
+        user_dict = get_user_by_username(username)
+        return UserInDB(**user_dict)
+
+
+def fake_decode_token(token):  # тут должна быть функция  jwt с декодом
     user = get_user(get_db(), token)
     return user
 
 
-async def get_current_user(token: str = Depends(oauth2_scheme)):
+async def get_current_user(token: str = Depends(oauth2_scheme)):  # проверяем токен у текущего юзера
     user = fake_decode_token(token)
     if not user:
         raise HTTPException(
@@ -44,7 +44,7 @@ async def get_current_user(token: str = Depends(oauth2_scheme)):
 
 
 async def get_current_active_user(
-    current_user: User = Depends(get_current_user)
+    current_user: User = Depends(get_current_user)  # is_active True or False
 ):
     if current_user.disabled:
         raise HTTPException(status_code=400, detail="Inactive user")
