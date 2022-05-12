4f44f43fdb589c3df594b6e30e0774e58214e1d1
Ninefiveblade <cherep1.92@yandex.ru>
2022-05-12 09:05:42 +0300

    test

--- fastpoet/users/routers.py
+++ fastpoet/users/routers.py
@@ -1,12 +1,16 @@
 """Routing module for users"""
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 
 from fastpoet.settings import models
 from fastpoet.settings.database import SessionLocal, engine
 
 from .schemas import User, UserCreate
-from .service import add_user, get_user, get_user_by_username, get_users
+from .service import add_user, get_user, get_users
+from .security import (
+    get_password_hash, UserInDB, get_current_active_user
+)
 
 router = APIRouter()
 
@@ -35,14 +39,30 @@ def user_get(user_id: int, db: Session = Depends(get_db)):
     return user
 
 
-@router.get("/user/{username}", response_model=User)
-def user_get(username: str, db: Session = Depends(get_db)):
-    """Get user by username"""
-    user = get_user_by_username(db, username)
-    return user
-
-
 @router.post("/users/", response_model=User)
 def create_user(user: UserCreate, db: Session = Depends(get_db)):
     """Create new user"""
     return add_user(db=db, user=user)
+
+
+@router.get("/test/")
+def get_token(token: str = Depends(OAuth2PasswordBearer(tokenUrl="token"))):
+    return {"token": token}
+
+
+@router.post("/token")
+async def login(form_data: OAuth2PasswordRequestForm = Depends()):
+    user_dict = get_db().get(form_data.username)
+    if not user_dict:
+        raise HTTPException(status_code=400, detail="Incorrect username or password")
+    user = UserInDB(**user_dict)
+    hashed_password = get_password_hash(form_data.password)
+    if not hashed_password == user.hashed_password:
+        raise HTTPException(status_code=400, detail="Incorrect username or password")
+
+    return {"access_token": user.username, "token_type": "bearer"}
+
+
+@router.get("/users/me")
+async def read_users_me(current_user: User = Depends(get_current_active_user)):
+    return current_user
--- fastpoet/users/security.py
+++ fastpoet/users/security.py
@@ -1,7 +1,51 @@
 from passlib.context import CryptContext
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+from sqlalchemy.orm import Session
+
+from .service import get_user_by_username
+from fastpoet.settings import models
+from .routers import get_db
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+User = models.User
 
 
 def get_password_hash(password: str):
     return pwd_context.hash(password)
+
+
+def get_user(username: str, db: Session = Depends(get_db)):
+    if username in db:
+        user_dict = get_user_by_username(username)
+        return UserInDB(**user_dict)
+
+
+class UserInDB(User):
+    hashed_password: str
+
+
+def fake_decode_token(token):
+    user = get_user(get_db(), token)
+    return user
+
+
+async def get_current_user(token: str = Depends(oauth2_scheme)):
+    user = fake_decode_token(token)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid authentication credentials",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    return user
+
+
+async def get_current_active_user(
+    current_user: User = Depends(get_current_user)
+):
+    if current_user.disabled:
+        raise HTTPException(status_code=400, detail="Inactive user")
+    return current_user
