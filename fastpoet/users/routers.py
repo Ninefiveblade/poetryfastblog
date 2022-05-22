184903fd7f6c6814454388a52f6bb2edaca91af2
Ninefiveblade <cherep1.92@yandex.ru>
2022-05-22 23:21:23 +0300

    Создание ветки Auth

--- fastpoet/users/routers.py
+++ fastpoet/users/routers.py
@@ -32,7 +32,7 @@ def user_get(user_id: int, db: Session = Depends(get_db)) -> User:
 
 @router.post("/users/", response_model=User)
 def create_user(user: UserCreate, db: Session = Depends(get_db)) -> User:
-    """Create new user"""
+    """Create new user."""
     if get_user_by_username(db, user.username):
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
