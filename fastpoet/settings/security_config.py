d6835dc8ba89494bd81a303133ae9642165ac4ab
LeAries <learies@ya.ru>
2022-05-26 19:10:58 +0300

    Для SECRET_KEY добавил ключ по умолчанию
    Ключ по умолчанию нужен если нет .env.

--- fastpoet/settings/security_config.py
+++ fastpoet/settings/security_config.py
@@ -4,6 +4,6 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = os.getenv('SECRET_KEY') or 'you-will-never-guess'
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
