2e01775407d1ad057965d51d7086265923e45c2e
Ninefiveblade <cherep1.92@yandex.ru>
2022-05-29 21:09:37 +0300

    added groups models to mogrations, fix errors

--- migrations/env.py
+++ migrations/env.py
@@ -6,7 +6,8 @@ from sqlalchemy import pool
 from alembic import context
 
 from fastpoet.users import models as post_models
-from fastpoet.posts import models
+from fastpoet.posts import models as users_models
+from fastpoet.groups import models as group_models
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -22,7 +23,8 @@ if config.config_file_name is not None:
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
 target_metadata = post_models.Base.metadata
-target_metadata = models.Base.metadata
+target_metadata = users_models.Base.metadata
+target_metadata = group_models.Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
