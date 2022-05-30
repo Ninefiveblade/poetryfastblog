7b5318ee008805b80120a7c9243fec577a3f02e4
LeAries <learies@ya.ru>
2022-05-30 10:10:24 +0300

    Поправил форматирование в routers posts

--- fastpoet/posts/routers.py
+++ fastpoet/posts/routers.py
@@ -1,25 +1,30 @@
-"""Routing module for posts"""
-from fastapi import APIRouter, Depends, HTTPException
+"""Routing module for posts."""
+from typing import List
+
+from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm import Session
 
-from fastpoet.settings.database import engine, get_db
+from fastpoet.groups.service import get_group
+from fastpoet.posts.models import Post
+from fastpoet.settings.database import get_db
 
-from ..groups.service import get_group
-from .models import Post
 from .schemas import PostCreate, PostList
 from .service import create_post, get_posts
 
 router = APIRouter()
 
 
-@router.get("/posts/", response_model=list[PostList])
-def posts_get(db: Session = Depends(get_db)):
-    posts = get_posts(db)
-    return posts
+@router.get(
+    "/posts/", response_model=List[PostList], status_code=status.HTTP_200_OK
+)
+def posts_get(db: Session = Depends(get_db)) -> List[PostList]:
+    return get_posts(db)
 
 
-@router.post("/posts/", response_model=PostCreate)
-def post_create(post: PostCreate, db: Session = Depends(get_db)):
+@router.post(
+    "/posts/", response_model=PostCreate, status_code=status.HTTP_201_CREATED
+)
+def post_create(post: PostCreate, db: Session = Depends(get_db)) -> Post:
     """Create new post"""
     if post.group_id:
         if not get_group(db, post.group_id):
@@ -27,5 +32,4 @@ def post_create(post: PostCreate, db: Session = Depends(get_db)):
                 status_code=404,
                 detail=f"Группа под номером: {post.group_id} не существует.")
     # Нужна проверка что переданный author_id есть в db
-    post = create_post(db, post)
-    return post
+    return create_post(db, post)
