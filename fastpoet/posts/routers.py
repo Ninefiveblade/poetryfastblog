632b63b74d9ef4ae700e0fb986885c71b68cb056
LeAries <learies@ya.ru>
2022-06-03 21:10:09 +0300

    Добавил docstring и поправил форматирование

--- fastpoet/posts/routers.py
+++ fastpoet/posts/routers.py
@@ -19,6 +19,7 @@ router = APIRouter()
     "/posts/", response_model=List[PostList], status_code=status.HTTP_200_OK
 )
 def posts_get(db: Session = Depends(get_db)) -> List[PostList]:
+    """Get post list."""
     return get_posts(db)
 
 
@@ -26,15 +27,15 @@ def posts_get(db: Session = Depends(get_db)) -> List[PostList]:
     "/posts/", response_model=PostCreate, status_code=status.HTTP_201_CREATED
 )
 def post_create(post: PostCreate, db: Session = Depends(get_db)) -> Post:
-    """Create new post"""
+    """Create new post."""
     if post.group_id:
         if not get_group(db, post.group_id):
             raise HTTPException(
                 status_code=404,
-                detail=f"Группа под номером: {post.group_id} не существует.")
+                detail=f"Group number: {post.group_id} does not exist.")
     if not get_user(db, post.author_id):
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
-            detail=f"Автора под номером: {post.author_id} не существует.",
+            detail=f"Author number: {post.author_id} does not exist.",
         )
     return create_post(db, post)
