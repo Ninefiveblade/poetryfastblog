a322f2ce5504b5a31f4f0c2153b9adb958a40e9a
LeAries <learies@ya.ru>
2022-06-03 20:47:00 +0300

    Удалил user role из schemas

--- fastpoet/users/schemas.py
+++ fastpoet/users/schemas.py
@@ -6,63 +6,6 @@ from pydantic import BaseModel, EmailStr, Field, validator
 from fastpoet.posts.schemas import PostList
 
 
-# в стадии разработки
-class RoleBase(BaseModel):
-    name: Optional[str]
-    description: Optional[str]
-
-
-class RoleCreate(RoleBase):
-    pass
-
-
-class RoleUpdate(RoleBase):
-    pass
-
-
-class RoleInDBBase(RoleBase):
-    id: int
-
-    class Config:
-        orm_mode = True
-
-
-class Role(RoleInDBBase):
-    pass
-
-
-class RoleInDB(RoleInDBBase):
-    pass
-
-
-class UserRoleBase(BaseModel):
-    user_id: Optional[int]
-    role_id: Optional[int]
-
-
-class UserRoleCreate(UserRoleBase):
-    pass
-
-
-class UserRoleUpdate(BaseModel):
-    role_id: int
-
-
-class UserRoleInDBBase(UserRoleBase):
-    role: Role
-
-    class Config:
-        orm_mode = True
-
-
-class UserRole(UserRoleInDBBase):
-    pass
-
-
-class UserRoleInDB(UserRoleInDBBase):
-    pass
-
-
 class UserBase(BaseModel):
     """Base user pydantic model."""
     id: int = None
@@ -78,7 +21,6 @@ class UserBaseInDB(UserBase):
     first_name: Optional[str]
     last_name: Optional[str]
     born_year: Optional[int]
-    user_role: Optional[UserRole]
     posts: List[PostList] = []
 
 
@@ -107,7 +49,6 @@ class UserCreate(UserBaseInDB):
         min_length=8,
         regex="[a-zA-Z0-9]+([_ -]?[a-zA-Z0-9])*$"
     )
-    #  user_role: Optional[UserRole]
 
     @validator('username')
     def username_alphanumeric(cls, var):
@@ -153,6 +94,3 @@ class Token(BaseModel):
 class TokenData(BaseModel):
     """Create token data for current_user depends."""
     username: Union[str, None] = None
-
-
-#  Схемы ролей и юзерролей
