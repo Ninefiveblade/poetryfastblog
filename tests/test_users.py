61f3fbcfda91478647b9b3ed19fa5b1fb05b9ad2
LeAries <learies@ya.ru>
2022-06-22 14:44:41 +0300

    Обновил test_users

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,4 +1,6 @@
 """Tests for users models, endpoints."""
+from http import HTTPStatus
+
 from fastapi.testclient import TestClient
 
 from tests.conftest import app
@@ -8,23 +10,28 @@ client = TestClient(app)
 
 def test_users_enpoinds_active(test_db):
     response = client.get("/users/")
-    assert response.status_code == 200, response.text
+    assert response.status_code == HTTPStatus.OK, response.text
 
 
-def test_crud_user(test_db):
+def test_create_user(test_db):
     response = client.post(
         "/auth/signup/", json={"username": "testname", "password": "test1234"}
     )
-    assert response.status_code == 201, response.text
-    assert response.json()["username"] == "testname"
+    assert response.status_code == HTTPStatus.CREATED, response.text
+    assert response.json().get("username") == "testname"
     assert "id" in response.json()
-    user_id = response.json()["id"]
-    username = response.json()["username"]
 
+
+def test_get_user(test_db):
+    response = client.post(
+        "/auth/signup/", json={"username": "testname", "password": "test1234"}
+    )
+    user_id = response.json().get("id")
+    username = response.json().get("username")
     response = client.get(f"/users/{username}")
-    assert response.status_code == 200, response.text
-    assert response.json()["username"] == "testname"
-    assert response.json()["id"] == user_id
+    assert response.status_code == HTTPStatus.OK, response.text
+    assert response.json().get("username") == "testname"
+    assert response.json().get("id") == user_id
     assert not response.json()["posts"]
 
 
