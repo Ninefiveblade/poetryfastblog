910958e0873fa14b607063a2e8d47b2a6a1e611f
LeAries <learies@ya.ru>
2022-06-24 15:33:01 +0300

    Добавил user_fixture в тесты

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,6 +1,7 @@
 """Tests for users models, endpoints."""
 from http import HTTPStatus
 
+import pytest
 from fastapi.testclient import TestClient
 
 from tests.conftest import app
@@ -8,29 +9,32 @@ from tests.conftest import app
 client = TestClient(app)
 
 
+@pytest.fixture
+def user_fixture():
+    response = client.post(
+        "/auth/signup/", json={"username": "testname", "password": "test1234"}
+    )
+    return response
+
+
 def test_users_enpoinds_active(test_db):
     response = client.get("/users/")
     assert response.status_code == HTTPStatus.OK, response.text
+    assert isinstance(response.json(), list)
 
 
-def test_create_user(test_db):
-    response = client.post(
-        "/auth/signup/", json={"username": "testname", "password": "test1234"}
-    )
-    assert response.status_code == HTTPStatus.CREATED, response.text
-    assert response.json().get("username") == "testname"
-    assert "id" in response.json()
+def test_create_user(test_db, user_fixture):
+    assert user_fixture.status_code == HTTPStatus.CREATED, user_fixture.text
+    assert user_fixture.json().get("username") == "testname"
+    assert "id" in user_fixture.json()
 
 
-def test_get_user(test_db):
-    response = client.post(
-        "/auth/signup/", json={"username": "testname", "password": "test1234"}
-    )
-    user_id = response.json().get("id")
-    username = response.json().get("username")
+def test_get_user_by_username(test_db, user_fixture):
+    user_id = user_fixture.json().get("id")
+    username = user_fixture.json().get("username")
     response = client.get(f"/users/{username}")
     assert response.status_code == HTTPStatus.OK, response.text
-    assert response.json().get("username") == "testname"
+    assert response.json().get("username") == username
     assert response.json().get("id") == user_id
     assert not response.json().get("posts")
 
